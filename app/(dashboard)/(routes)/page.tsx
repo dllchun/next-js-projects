7fe3666e6ffd594d67606f40d691d9063edf0081
dllchun <wingchuncheung0108@gmail.com>
2023-10-30 15:03:15 +0800

    day 02

---
+++ app/(dashboard)/(routes)/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <>
+      <div className="text-3xl font-medium text-sky-700">
+        this is a protected page
+      </div>
+    </>
+  );
+}
--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import {Button} from "@/components/ui/button"
-
-export default function Home() {
-  return (
-    <>
-   <div className="text-3xl font-medium text-sky-700">this is home page</div> 
-   <Button variant="destructive">Click Me</Button>
-   </> )
-}
